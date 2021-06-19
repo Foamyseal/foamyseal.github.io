b5850a3f174fdc346292dc7ccbbcdb26a4b8718c
Martin Au-yeung <you@example.com>
2021-06-19 12:33:28 -0700

    update SEO and meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,22 @@ import { Footer } from "../components/footer";
 import { Intro } from "../components/intro";
 import { Projects } from "../components/projects/projects";
 
+const appTitle = "Martin Au-yeung";
+const description = "Martin Au-yeung's cool portfolio site. Built with NextJS";
+
 const App = () => {
   return (
     <>
       <Head>
-        <title>Martin Au-yeung</title>
+        <title>{appTitle}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'></link>
+        <meta name="description" content={description}></meta>
+        <meta property="og:title" content={appTitle} key="ogtitle" />
++       <meta property="og:description" content={description} key="ogdesc" />
+        <meta property="og:url" content="https://martinauyeung.com/" />
+        <meta property="og:type" content="website" /> 
+        <meta property="og:image" content="http://martinauyeung.com/martinauyeungimage.png" key="ogimage" />
       </Head>
       <div className="pb-8">
           <Header />
