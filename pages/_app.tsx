a6b4531cf643b6f2e2df3fd5546adb7103e3cb37
foamyseal <martin.red.202@gmail.com>
2022-01-31 16:00:41 -0800

    retry google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,36 @@
 import 'tailwindcss/tailwind.css'
 import { useEffect } from "react";
+import Script from 'next/script'
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import * as gtag from "../util/gtag";
+import { GTM_ID, pageview } from '../lib/gtm'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-
+  const router = useRouter()
   useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on('routeChangeComplete', pageview)
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+      router.events.off('routeChangeComplete', pageview)
+    }
+  }, [router.events])
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', '${GTM_ID}');
+      `,
+        }}
+      />
+      <Component {...pageProps} />)
+    </>
+  )
 };
 
 export default App;
